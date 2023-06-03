7efebc83e6d43c414fb6c080bbdcf72866d73ad7
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-06-03 15:11:35 +0300

    feat

--- src/commands/Files/index.ts
+++ src/commands/Files/index.ts
@@ -1,6 +1,15 @@
-import { Markup, Telegraf } from "telegraf";
+import { Context, Markup, Telegraf } from "telegraf";
 import { Command } from "../Command";
 import { ILogger } from "../../logger";
+
+const SERVICES = {
+    ui8net: 'ui8net',
+    craftwork: 'craftwork',
+    ls_graphics: 'ls_graphics',
+    uihunt: 'uihunt',
+    pixsellz: 'pixsellz',
+    spline: 'spline'
+}
 
 export class Files extends Command {
     constructor(bot: Telegraf, logger: ILogger) {
@@ -24,21 +33,36 @@ export class Files extends Command {
                    parse_mode: 'MarkdownV2',
                    ...Markup.inlineKeyboard([ 
                     [
-                        Markup.button.callback('UI8NET', 'ui8net'),
-                        Markup.button.callback('CRAFTWORK', 'craftwork')
+                        Markup.button.callback('UI8NET', SERVICES.ui8net),
+                        Markup.button.callback('CRAFTWORK', SERVICES.craftwork)
                     ],
                     [
-                        Markup.button.callback('LS. Graphics', 'ls_graphics'),
-                        Markup.button.callback('UIHUNT', 'uihunt')
+                        Markup.button.callback('LS. Graphics', SERVICES.ls_graphics),
+                        Markup.button.callback('UIHUNT', SERVICES.uihunt)
                     ],
                     [
-                        Markup.button.callback('Pixsellz', 'pixsellz'),
-                        Markup.button.callback('Spline.one', 'spline'),
+                        Markup.button.callback('Pixsellz', SERVICES.pixsellz),
+                        Markup.button.callback('Spline.one', SERVICES.spline),
                     ],
                     [Markup.button.callback('Назад', 'menu')]
                   ]) 
                 }
            )
+        })
+
+        this.bot.action([
+            SERVICES.ui8net,
+            SERVICES.craftwork,
+            SERVICES.ls_graphics,
+            SERVICES.uihunt,
+            SERVICES.pixsellz,
+            SERVICES.spline,
+        ], ctx => {
+            console.log('SELECT SERVICE')
+        })
+
+        this.bot.action('menu', async (ctx) => {
+            this.sendMenu(ctx)
         })
     }
 }
